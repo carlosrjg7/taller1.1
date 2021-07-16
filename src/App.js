c96fb9da9f2f373f5e702a9acfd1431c8574147e
Carlos Garcia <cgarcia@dtransforma.com>
2021-07-16 18:34:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Parrafo from './components/Parrafo';
 
 function App() {
+
+  const lista = ['Cesar','Karen','Carlos','Andree','Efrain','Raul'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        lista.map((nombre,index) => (
+          <Parrafo name={nombre} numero={index} key={index} />
+        ))
+      }
     </div>
   );
 }
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,16 @@
+import React,{Fragment} from 'react';
+
+
+const Parrafo = (props) => {
+
+    const {name,numero} = props;
+
+    return (
+        <Fragment>
+            <h1>hola</h1>
+            <h2>hola</h2>
+        </Fragment>
+    )
+}
+
+export default Parrafo
